7e531de244b1c88ba3999eb263173bbc71f502ac
wenchongxu <xu000192@algonquinlive.com>
2018-03-19 12:11:51 -0400

    create patientactivity

---
+++ app/src/main/java/com/algonquin/finalgroupproject/PatientIntakeFormActivity.java
@@ -0,0 +1,13 @@
+package com.algonquin.finalgroupproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PatientIntakeFormActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_intake_form);
+    }
+}
