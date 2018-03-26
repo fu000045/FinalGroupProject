fa423dab86e2dfe1400ab4172495a199a23dbb99
zhubo <zhu00069@algonquinlive.com>
2018-03-26 13:40:14 -0400

    bo-movie -1st demo

---
+++ app/src/main/java/com/algonquin/finalgroupproject/Movie2.java
@@ -0,0 +1,13 @@
+package com.algonquin.finalgroupproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Movie2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie2);
+    }
+}
