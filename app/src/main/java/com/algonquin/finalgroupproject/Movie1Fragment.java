32c228640a6b2aea2046039d6d231f11d0211a54
zhubo <zhu00069@algonquinlive.com>
2018-04-08 01:28:31 -0400

    bo-movie -add new features

--- app/src/main/java/com/algonquin/finalgroupproject/Movie1Fragment.java
+++ app/src/main/java/com/algonquin/finalgroupproject/Movie1Fragment.java
@@ -1,15 +1,88 @@
 package com.algonquin.finalgroupproject;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Movie1Fragment extends Fragment {
-    public View onCreate(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        inflater.inflate(R.layout.activity_movie1_detail, null);
-         return super.onCreateView(inflater, container, savedInstanceState);
+   public boolean isTablet;
+    TextView txt_movieTitle;
+    TextView txt_mainActors;
+    TextView txt_length;
+    TextView txt_genre;
+    TextView txt_description;
+    TextView txt_id;
+    Button btn_delete;
+    Button btn_update;
+    Bundle bundle;
+
+ @Nullable
+ @Override
+ public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+  View view = inflater.inflate(R.layout.movie_fragment_layout, container, false);
+
+  txt_movieTitle = view.findViewById(R.id.movie_title_framelayout);
+  txt_mainActors = view.findViewById(R.id.movie_actor_framelayout);
+  txt_length = view.findViewById(R.id.movie_length_framelayout);
+  txt_genre = view.findViewById(R.id.movie_genre_framelayout);
+  txt_description = view.findViewById(R.id.movie_description_framelayout);
+  txt_id = view.findViewById(R.id.id_fragment);
+  btn_delete = view.findViewById(R.id.delete_movie);
+  btn_update = view.findViewById(R.id.update_movie);
+
+  //Get message passed by tablet(chatwindow) or phone(messagedetails)
+  bundle = getArguments();
+
+  String movieTitle = bundle.getString("Movie Title");
+  String mainActors = bundle.getString("Main Actors");
+  String length = bundle.getString("Length");
+  String genre = bundle.getString("Genre");
+  String description = bundle.getString("Description");
+  final long id = bundle.getLong("ID");
+  final long id_movie = bundle.getLong("IDmovie");
+
+  txt_movieTitle.setText(movieTitle);
+  txt_mainActors.setText(mainActors);
+  txt_length.setText(length);
+  txt_genre.setText(genre);
+  txt_description.setText(description);
+  txt_id.setText(String.valueOf(id));
+
+
+  btn_delete.setOnClickListener(new View.OnClickListener() {
+   @Override
+   public void onClick(View view) {
+    if(isTablet) {
+     Movie1 m1 = (Movie1)getActivity();
+    // m1.deleteForTablet(id, id_movie);
+     getFragmentManager().beginTransaction().remove(Movie1Fragment.this).commit();
+    } else {
+     Intent resultIntent = new Intent();
+     resultIntent.putExtra("DeleteID", id);
+     resultIntent.putExtra("IDMovie", id_movie);
+     getActivity().setResult(Activity.RESULT_OK, resultIntent);
+     getActivity().finish();
     }
+   }
+  });
+
+  return view;
+ }
+
+ @Override
+ public void onCreate(@Nullable Bundle savedInstanceState) {
+  super.onCreate(savedInstanceState);
+ }
+
+ public void setIsTablet(boolean b){
+  isTablet = b;
+ }
 }
--- app/src/main/java/com/algonquin/finalgroupproject/MovieActivity.java
+++ app/src/main/java/com/algonquin/finalgroupproject/MovieActivity.java
@@ -1,17 +1,24 @@
 package com.algonquin.finalgroupproject;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 public class MovieActivity extends Activity {
     LinearLayout linearlayout;
     Button btn_movie1;
-    Button btn_movie2;
+    Button btn_button_website;
+    Button btn_button_statistic;
+    Button btn_button_help;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +26,11 @@ public class MovieActivity extends Activity {
         setContentView(R.layout.activity_movie);
 
         linearlayout = findViewById(R.id.linearlayout);
-        btn_movie1 = findViewById(R.id.Movie1);
-        btn_movie2 = findViewById(R.id.Movie2);
+        btn_movie1 = findViewById(R.id.btn_movie_all);
+        btn_button_website = findViewById(R.id.btn_movieFromWebsite);
+        btn_button_statistic = findViewById(R.id.btn_movie_statistic);
+        btn_button_help = findViewById(R.id.btn_help);
+
 
         //From: Android Snackbar Example Tutorial [Web Page]
         //Retrieved from: https://www.journaldev.com/10324/android-snackbar-example-tutorial
@@ -36,18 +46,43 @@ public class MovieActivity extends Activity {
                 startActivity(intent);
             }
         });
-        btn_movie2.setOnClickListener(new View.OnClickListener() {
+        btn_button_website.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar snackbar = Snackbar
-                        .make(linearlayout, "Welcome movie2", Snackbar.LENGTH_LONG);
+                        .make(linearlayout, "Loading movies from XML", Snackbar.LENGTH_LONG);
 
                 snackbar.show();
-                Intent intent = new Intent(MovieActivity.this, Movie2.class);
+                Intent intent = new Intent(MovieActivity.this, MovieFromWebActivity.class);
                 startActivity(intent);
             }
         });
-        //Jump to Movie2.
+
+        btn_button_statistic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btn_button_help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater inflater = MovieActivity.this.getLayoutInflater();
+
+                //Pop up a dialogue box.
+                AlertDialog.Builder builder = new AlertDialog.Builder(MovieActivity.this);
+                builder.setView(inflater.inflate(R.layout.help_layout, null));
+                // 2. Chain together various setter methods to set the dialog characteristics
+                builder.setTitle(R.string.help)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        })
+                        .show();
+            }
+        });
 
     }
 }
