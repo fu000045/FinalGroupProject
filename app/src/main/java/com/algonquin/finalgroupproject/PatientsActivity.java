d0c85414eb6492730a27dd9cd1a4c3159b00a7c9
wenchongxu <xu000192@algonquinlive.com>
2018-04-10 18:20:09 -0400

    adding data to list

--- app/src/main/java/com/algonquin/finalgroupproject/PatientsActivity.java
+++ app/src/main/java/com/algonquin/finalgroupproject/PatientsActivity.java
@@ -1,106 +1,98 @@
 package com.algonquin.finalgroupproject;
-
 import android.app.Activity;
-import android.app.FragmentTransaction;
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-
+import android.widget.TextView;
 import java.util.ArrayList;
-
 public class PatientsActivity extends Activity {
 
     Button btn_doctor;
     Button btn_dentist;
     Button btn_optometrist;
-    ListView listViewDoc;
+    ListView listView;
     ListView listViewDen;
     ListView listViewOpt;
-    EditText editTextName;
-    EditText editTextAddr;
-    EditText editTextBirt;
-    EditText editTextPhon;
-    EditText editTextHeal;
-    EditText editTextDoc;
-    EditText editTextDen;
-    EditText editTextOpt;
-    ArrayList<String> listDoc = new ArrayList<>();
-    ArrayList<String> listDen = new ArrayList<>();
-    ArrayList<String> listOpt = new ArrayList<>();
-    Cursor cursor;
-    PatientDatabaseHelper patientDatabaseHelper;
+
+    ArrayList<String> list = new ArrayList<>();
+
     PatientAdapter docAdapter;
-    PatientAdapter denAdapter;
-    PatientAdapter optAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patients);
         docAdapter = new PatientAdapter(this);
-        denAdapter = new PatientAdapter(this);
-        optAdapter = new PatientAdapter(this);
 
-        patientDatabaseHelper = new PatientDatabaseHelper(this);
-        final SQLiteDatabase db = patientDatabaseHelper.getWritableDatabase();
+        listView = findViewById(R.id.list_view);
+//        listViewDen = findViewById(R.id.list_dentist);
+//        listViewOpt = findViewById(R.id.list_optometrist);
+        btn_doctor=findViewById(R.id.btn_doctor);
+        btn_dentist=findViewById(R.id.btn_dentist);
+        btn_optometrist=findViewById(R.id.btn_optometrist);
 
-        //prepare for doctor---to add the data which already have in the list
-        String selectQuery = "SELECT  * FROM " + "doctorTable" ;
-        cursor = db.rawQuery(selectQuery,null);
-        cursor.moveToFirst();
-        while(!cursor.isAfterLast()){
-            listDoc.add(cursor.getString(cursor.getColumnIndex(PatientDatabaseHelper.KEY_NAME)));
-            listDoc.add(cursor.getString(cursor.getColumnIndex(PatientDatabaseHelper.KEY_BOD)));
-            listDoc.add(cursor.getString(cursor.getColumnIndex(PatientDatabaseHelper.KEY_PHONE)));
-            listDoc.add(cursor.getString(cursor.getColumnIndex(PatientDatabaseHelper.KEY_ADDRESS)));
-            listDoc.add(cursor.getString(cursor.getColumnIndex(PatientDatabaseHelper.KEY_CARD)));
-            listDoc.add(cursor.getString(cursor.getColumnIndex(PatientDatabaseHelper.KEY_DOC)));
-            cursor.moveToNext();
-        }
-        //Log.i(ACTIVITY_NAME, "Cursor's  column count =" + cursor.getColumnCount());
-        //listView.setAdapter (messageAdapter);
+
+        listView.setAdapter (docAdapter);
         docAdapter.notifyDataSetChanged();
 
-
-        //add new patient to the database
-        btn_doctor=findViewById(R.id.btn_doctor);
         btn_doctor.setOnClickListener(new View.OnClickListener() {
                                           @Override
                                           public void onClick(View view) {
-                                              listDoc.add(editTextName.getText().toString());
-                                              ContentValues cValues = new ContentValues();
-                                              cValues.put(PatientDatabaseHelper.KEY_NAME, editTextName.getText().toString());
-                                              db.insert("doctorTable", null, cValues);
-                                              listViewDoc.setAdapter(docAdapter);
-                                              docAdapter.notifyDataSetChanged();
-                                              editTextName.setText("");
+                                              Intent intent = new Intent(PatientsActivity.this, DoctorFragment.class);
+                                         startActivity(intent);
                                           }
+                                      });
+
+        btn_dentist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PatientsActivity.this, DoctorFragment.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_optometrist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PatientsActivity.this, DoctorFragment.class);
+                startActivity(intent);
+            }
+        });
+
+        //add new patient to the database
+//        btn_doctor.setOnClickListener(new View.OnClickListener() {
+//                                          @Override
+//                                          public void onClick(View view) {
+//                                              listDoc.add(editTextName.getText().toString());
+//                                              ContentValues cValues = new ContentValues();
+//                                              cValues.put(PatientDatabaseHelper.KEY_NAME, editTextName.getText().toString());
+//                                              db.insert("doctorTable", null, cValues);
+//                                             // listViewDoc.setAdapter(docAdapter);
+//                                              docAdapter.notifyDataSetChanged();
+//                                              editTextName.setText("");
+//                                              cursor = db.rawQuery("SELECT  * FROM " + "doctorTable" ,null);
+//                                              Toast.makeText(PatientsActivity.this, "Successfully added!", Toast.LENGTH_SHORT).show();
+//                                             // finish();
+//                                          }
 //                messageAdapter.notifyDataSetChanged(); //this restarts the process of getCount() & getView()
 //                editText.setText("");
 //                cursor = db.query(false, ChatDatabaseHelper.TABLE_NAME,
 //                        new String[] { ChatDatabaseHelper.KEY_ID, ChatDatabaseHelper.KEY_MESSAGE }, null, null, null, null, null, null);
-//            }
-                                          });
-                                      }
-
-
-//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long id) {
-//                Bundle bundle = new Bundle();
-//                //  bundle.putBoolean("isTablet",isTablet);
-//                bundle.putLong("ID",id);
-//                bundle.putString("Message", messageAdapter.getItem(i));
+                               //           });
+     listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long id) {
+                Bundle bundle = new Bundle();
+                //  bundle.putBoolean("isTablet",isTablet);
+                bundle.putLong("ID",id);
+                bundle.putString("Message", docAdapter.getItem(i));
 //                if(isTablet){
 //                    frag = new MessageFragment(ChatWindow.this);
 //                    frag.setArguments(bundle);
@@ -108,19 +100,42 @@ public class PatientsActivity extends Activity {
 //                    FragmentTransaction transaction = getFragmentManager().beginTransaction();
 //                    transaction.replace(R.id.frameLayout, frag).commit();
 //                }
-//                else {
-//                    Intent intent = new Intent(ChatWindow.this,MessageDetailsActivity.class);
-//                    intent.putExtra("ID", id);
-//                    intent.putExtra("Message", messageAdapter.getItem(i));//pass the Database ID and message to next activity
-//                    startActivityForResult(intent,5);
-//                }
-//            }
-//        });
+              //  else {
+                    Intent intent = new Intent(PatientsActivity.this,PatientDetails.class);
+                    intent.putExtra("ID", id);
+                    intent.putExtra("Message", docAdapter.getItem(i));//pass the Database ID and message to next activity
+                    startActivityForResult(intent,5);
+              //  }
+            }
+        });
+    }
 
 
     private class PatientAdapter extends ArrayAdapter<String> {
         public PatientAdapter(Context ctx){super(ctx,0);}
+//        public int getCount(){
+//            return listDoc.size();
+//        }
+        public String getItem(int position){
+            return list.get(position);
+        }
 
+        public View getView(int position, View convertView, ViewGroup parent){
+            LayoutInflater inflater = PatientsActivity.this.getLayoutInflater();
+            View result;
+           // if(position%2 == 0)
+                result = inflater.inflate(R.layout.doctor_layout, null);
+//            else
+//                result = inflater.inflate(R.layout.chat_row_outgoing, null);
+            TextView message = result.findViewById(R.id.text_view_name);
+            message.setText(  getItem(position)  ); // get the string at position
+            return result;
+        }
 
     }
+
+    @Override
+    public void onDestroy(){
+      //  cursor.close();
+        super.onDestroy(); }
 }
