c394c1fdc7bcb025ce91a29b040bbacafa636dfc
allengoo <guav3a@gmail.com>
2018-04-14 16:34:00 -0400

    OC Transpo #1

--- app/src/main/java/com/algonquin/finalgroupproject/MainActivity.java
+++ app/src/main/java/com/algonquin/finalgroupproject/MainActivity.java
@@ -1,14 +1,15 @@
 package com.algonquin.finalgroupproject;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.algonquin.finalgroupproject.octranspo.OCBusNumberWindow;
 
 public class MainActivity extends AppCompatActivity {
     private Button btn_quizCreater;
@@ -22,13 +23,13 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
- //       ActionBar ab = getSupportActionBar();
- //      ab.setDisplayHomeAsUpEnabled(true);
+        //       ActionBar ab = getSupportActionBar();
+        //      ab.setDisplayHomeAsUpEnabled(true);
 
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
@@ -38,11 +39,19 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.Movie:
 
-                        Toast.makeText(MainActivity.this, "Movie List Selected!", Toast.LENGTH_SHORT).show();
-                        //Jump to MovieActivity.
-                        Intent intent = new Intent(MainActivity.this, MovieActivity.class);
-                        startActivity(intent);
-                        break;
+                Toast.makeText(MainActivity.this, "Movie List Selected!", Toast.LENGTH_SHORT).show();
+                //Jump to MovieActivity.
+                Intent intent = new Intent(MainActivity.this, MovieActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.OCTrans:
+
+                Toast.makeText(MainActivity.this, getString(R.string.oc_toast), Toast.LENGTH_SHORT).show();
+                //Jump to MovieActivity.
+                Intent intent_oc = new Intent(MainActivity.this, OCBusNumberWindow.class);
+                startActivity(intent_oc);
+                break;
 
             default:
                 return super.onOptionsItemSelected(item);
